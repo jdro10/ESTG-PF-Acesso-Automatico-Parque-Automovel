32a5cefc3771dab4ad655105c261c19ed9e7debb
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-05-05 00:37:03 +0100

    created database tables

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -2,24 +2,84 @@ const db = require('../config/db');
 
 var dbController = {};
 
-dbController.createUserTable = function(req, res){
-    db.query(
-        "CREATE TABLE users(number INTEGER, name VARCHAR(40), plate VARCHAR(6) PRIMARY KEY, car_brand VARCHAR(20), car_model VARCHAR(20))", function(err, res){
-            if(err){
-                console.log(err);
-            } else{
-                console.log("Users table created successfully");
-            }
-        });
+dbController.createUserTable = function (req, res) {
+    const query = `
+        CREATE TABLE users (
+            number INTEGER,
+            firstName VARCHAR,
+            lastName VARCHAR,
+            PRIMARY KEY(number)
+        );
+        `;
+
+    db.query(query, function (err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Users table created successfully");
+        }
+    });
 };
 
-dbController.createParkingLotAccessTable = function(req, res){
-    db.query(
-        "CREATE TABLE parkAccess(plate VARCHAR(6), date DATE, FOREIGN KEY(plate) REFERENCES users(plate))", function(err, res){
-            if(err){
-                console.log(err);
-            }else{
-                console.log("Parking lot access table created successfully");
-            }
-        });
+dbController.createCarTable = function (req, res) {
+    const query = `
+        CREATE TABLE cars (
+            plate VARCHAR,
+            car_brand VARCHAR,
+            car_model VARCHAR,
+            PRIMARY KEY(plate)
+        );
+        `;
+
+    db.query(query, function (err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Car table created successfully");
+        }
+    });
 };
+
+dbController.createParkAccessTable = function (req, res) {
+    const query = `
+        CREATE TABLE parkAccess (
+            plate VARCHAR,
+            date DATE,
+            PRIMARY KEY(date),
+            FOREIGN KEY(plate) REFERENCES parkDriver(plate)
+        );
+        `;
+
+    db.query(query, function (err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Parking lot access table created successfully");
+        }
+    });
+};
+
+dbController.createParkDriverTable = function (req, res) {
+    const query = `
+        CREATE TABLE parkDriver (
+            number INTEGER,
+            plate VARCHAR UNIQUE,
+            PRIMARY KEY(number, plate),
+            FOREIGN KEY(number) REFERENCES users(number),
+            FOREIGN KEY(plate) REFERENCES cars(plate)
+        );
+        `;
+
+    db.query(query, function (err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Park driver table created successfully");
+        }
+    });
+};
+
+dbController.createUserTable();
+dbController.createCarTable();
+dbController.createParkDriverTable();
+dbController.createParkAccessTable();
