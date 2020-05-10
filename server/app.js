c54ae0b9a5449d8bd1918c4da3ff8673b9927814
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-05-10 18:39:43 +0100

    added open and closed park mode0

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var platesRouter = require('./routes/plates')
+var platesRouter = require('./routes/plates');
+var parkRouter = require('./routes/park');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/plates', platesRouter)
+app.use('/plates', platesRouter);
+app.use('/park', parkRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/routes/park.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var park = require('../controllers/parkController');
+
+router.get('/getParkMode', function(req, res, next){
+    park.getParkMode(req, res);
+})
+
+router.put('/changeParkMode', function(req, res, next) {
+    park.changeParkMode(req, res);
+});
+
+module.exports = router;
