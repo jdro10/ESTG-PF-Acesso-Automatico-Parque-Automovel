956ca9047a59f2a5c50899d28b81673633d6ff05
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-05-18 02:13:30 +0100

    added error handling

--- plate_detection/stream/exit_mjpeg_stream.py
+++ plate_detection/stream/exit_mjpeg_stream.py
@@ -8,19 +8,22 @@ cap = cv2.VideoCapture(0)
 
 class CamHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
-        self.end_headers()
-
-        while True:
-            ret, frame = cap.read()
-            ret2, jpeg = cv2.imencode('.jpeg', frame)
-          
-            self.wfile.write("--jpgboundary".encode())
-            self.send_header('Content-type', 'image/jpeg')
-            self.send_header('Content-length', str(len(jpeg)))
+        try:
+            self.send_response(200)
+            self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
             self.end_headers()
-            self.wfile.write(bytearray(jpeg))
+
+            while True:
+                ret, frame = cap.read()
+                ret2, jpeg = cv2.imencode('.jpeg', frame)
+            
+                self.wfile.write("--jpgboundary".encode())
+                self.send_header('Content-type', 'image/jpeg')
+                self.send_header('Content-length', str(len(jpeg)))
+                self.end_headers()
+                self.wfile.write(bytearray(jpeg))
+        except ConnectionResetError:
+            print("Conexão terminada.")
             
 
 try:
