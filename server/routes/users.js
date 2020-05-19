4c40d6144ba827dce77686af479b8bcbf57b9490
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-05-19 14:23:30 +0100

    added park acess query

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var user = require('../controllers/usersController');
 
+router.get('/', function(req, res, next) {
+  user.showAllUsersInfo(req, res);
+});
+
 router.post('/createUser', function(req, res, next){
   user.create(req, res);
 });
@@ -10,8 +14,8 @@ router.put('/updateUser', function(req, res, next){
   user.updateUserCar(req, res);
 });
 
-router.get('/', function(req, res, next) {
-  user.showAllUsersInfo(req, res);
+router.get('/parkAccess', function (req, res, next) {
+  user.showParkAccess(req, res);
 });
 
 router.get('/userInfo/:number', function(req, res, next) {
