3ffe4ec191370a0e8d45bfee45611e2c0ca488cc
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-05-21 22:08:48 +0100

    added new components

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,16 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ParkaccessComponent } from './parkaccess/parkaccess.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParkaccessComponent
+    ParkaccessComponent,
+    SidebarComponent,
+    NavbarComponent,
+    SignupComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,
--- webapp/src/app/parkaccess/parkaccess.component.ts
+++ webapp/src/app/parkaccess/parkaccess.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChange } from '@angular/core';
 import { ParkAccessService } from '../services/park-access.service';
-
 import { ParkAccess } from '../models/ParkAccess';
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-parkaccess',
@@ -9,18 +9,91 @@ import { ParkAccess } from '../models/ParkAccess';
   styleUrls: ['./parkaccess.component.css']
 })
 export class ParkaccessComponent implements OnInit {
+  
   parkAccess: ParkAccess[];
-  page = 1;
-  pageSize = 4;
-  collectionSize = 5;
+  date: NgbDateStruct;
+  todayDateS: string;
+  nDate: Date;
+  dateString: string;
+  count: number;
 
-  constructor(private parkAccessService: ParkAccessService) { }
+  constructor(private parkAccessService: ParkAccessService, private calendar: NgbCalendar) { }
 
   ngOnInit(): void {
-    this.parkAccessService.getParkAccess().subscribe(parkAccess => {
+    this.count = 0;
+    this.parkAccessService.getParkAccess(this.todayDate()).subscribe(parkAccess => {
       this.parkAccess = parkAccess;
     });
-    console.log(this.parkAccess);
   }
 
+  todayDate(): string {
+    this.date = this.calendar.getToday();
+
+    if (this.date.month >= 1 && this.date.month < 10) {
+      return this.todayDateS = this.date.year + '-0' + this.date.month + '-' + this.date.day;
+    } else {
+      return this.todayDateS = this.date.year + '-' + this.date.month + '-' + this.date.day;
+    }
+  }
+
+  traverseDate(): string {
+    this.count++;
+    this.nDate = new Date(this.date.year, this.date.month, this.date.day - this.count);
+
+    if (this.date.month >= 1 && this.date.month < 10) {
+      if (this.nDate.getDate() >= 1 && this.nDate.getDate() < 10) {
+        this.dateString = this.nDate.getFullYear() + "-0" + this.nDate.getMonth() + "-0" + this.nDate.getDate();
+      } else {
+        this.dateString = this.nDate.getFullYear() + "-0" + this.nDate.getMonth() + "-" + this.nDate.getDate();
+      }
+    } else {
+      if (this.nDate.getDate() >= 1 && this.nDate.getDate() < 10) {
+        this.dateString = this.nDate.getFullYear() + "-0" + this.nDate.getMonth() + "-0" + this.nDate.getDate();
+      } else {
+        this.dateString = this.nDate.getFullYear() + "-0" + this.nDate.getMonth() + "-" + this.nDate.getDate();
+      }
+    }
+
+    return this.dateString;
+  }
+
+  traverseDate2(): string {
+    this.count++;
+    this.nDate = new Date(this.date.year, this.date.month, this.date.day + this.count);
+
+    if (this.date.month >= 1 && this.date.month < 10) {
+      if (this.nDate.getDate() >= 1 && this.nDate.getDate() < 10) {
+        this.dateString = this.nDate.getFullYear() + "-0" + this.nDate.getMonth() + "-0" + this.nDate.getDate();
+      } else {
+        this.dateString = this.nDate.getFullYear() + "-0" + this.nDate.getMonth() + "-" + this.nDate.getDate();
+      }
+    } else {
+      if (this.nDate.getDate() >= 1 && this.nDate.getDate() < 10) {
+        this.dateString = this.nDate.getFullYear() + "-0" + this.nDate.getMonth() + "-0" + this.nDate.getDate();
+      } else {
+        this.dateString = this.nDate.getFullYear() + "-0" + this.nDate.getMonth() + "-" + this.nDate.getDate();
+      }
+    }
+    return this.dateString;
+  }
+
+  dayBefore(): void {
+    this.parkAccessService.getParkAccess(this.traverseDate()).subscribe(parkAccess => {
+      this.parkAccess = parkAccess;
+    });
+  }
+
+  nextDay(): void {
+    this.parkAccessService.getParkAccess(this.traverseDate2()).subscribe(parkAccess => {
+      this.parkAccess = parkAccess;
+    });
+  }
+
+  searchDate(): void {
+    var inputDate = (<HTMLInputElement>document.getElementById("dataSearch")).value;
+
+    this.parkAccessService.getParkAccess(inputDate).subscribe(parkAccess => {
+      this.parkAccess = parkAccess;
+    });
+  }
 }
