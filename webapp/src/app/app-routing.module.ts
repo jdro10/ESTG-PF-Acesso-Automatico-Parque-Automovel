8e08f1f6476d79b9420c38431d5cc466611e1e1f
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-05-31 16:31:30 +0100

    added login component

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { SignupComponent } from './signup/signup.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { UsersComponent } from './users/users.component';
 import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'users', component: UsersComponent },
   { path: 'userDetails/:numero', component: UserdetailsComponent},
+  { path: 'login', component: LoginComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
