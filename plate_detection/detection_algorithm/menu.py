8d1a1d63f6e96e65c89cb0c3f58843ef40b8f52b
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-07-08 15:13:56 +0100

    added error handling

--- plate_detection/detection_algorithm/menu.py
+++ plate_detection/detection_algorithm/menu.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from threading import Thread
 from park_access import ParkAccess
@@ -11,87 +12,98 @@ class Menu:
         self.exit_park_lights = None
         self.entry_plate_detection = None
         self.exit_plate_detection = None
-        self.thread_list = []
 
 
     def menu(self):
-        choice = '-1'
+        try:
+            print("DETEÇÃO DE MATRÍCULAS")
+            print("1 - Deteção de matrículas na entrada")
+            print("2 - Deteção de matrículas na saída")
+            print("0 - Sair")
+            print("Introduza a sua escolha (1/2/0): ")
+            choice = input()
 
-        while choice != '0':
-            try:
-                print("DETEÇÃO DE MATRÍCULAS")
-                print("1 - Deteção de matrículas na entrada")
-                print("2 - Deteção de matrículas na saída")
-                print("0 - Sair")
-                print("Introduza a sua escolha (1/2/0): ")
-                choice = input()
-
-                if choice == '1':
-                    try:
-                        self.entry_park_lights = ParkAccess("entry_queue_p")
-                        self.entry_plate_detection = PlateDetection("entry_queue", '192.168.1.120', 9000)
+            if choice == '1':
+                print("\nDeteção de matrículas iniciada na entrada.\n")
+                try:
+                    self.entry_park_lights = ParkAccess("entry_queue_p")
+                    self.entry_plate_detection = PlateDetection("entry_queue", '192.168.1.65', 9000)
                         
-                        thread1 = Thread(target=self.entry_plate_detection.read_stream)
-                        thread2 = Thread(target=self.entry_park_lights.loop)
+                    thread1 = Thread(target=self.entry_plate_detection.read_stream)
+                    thread2 = Thread(target=self.entry_park_lights.loop)
 
-                        thread1.start()
-                        thread2.start()
+                    thread1.start()
+                    thread2.start()
 
-                        self.thread_list.append(thread1)
-                        self.thread_list.append(thread2)
+                except AlprException:
+                    print("\nERRO: Verifique se o OpenALPR está corretamente instalado.\n")
+                    print("Programa terminado.")
+                    os._exit(0)
 
-                    except AlprException:
-                        print("\nERRO: Verifique se o OpenALPR está corretamente instalado.\n")
+                except ConnectionRefusedError:
+                    print("\nERRO: Verifique a ligação de rede com o computador responsável pela captura de imagens.\n")
+                    print("Programa terminado.")
+                    os._exit(0)
 
-                    except ConnectionRefusedError:
-                        print("\nERRO: Verifique a ligação de rede com o computador responsável pela captura de imagens.\n")
+                except IOError as e:
+                    if e.errno == 101:
+                        print("\nERRO: Verifique a sua conexão à rede.\n")
+                        print("Programa terminado.")
+                        os._exit(0)
 
-                elif choice == '2':
-                    try:
-                        self.exit_park_lights = ParkAccess("exit_queue_p")
-                        self.exit_plate_detection = PlateDetection("exit_queue", '192.168.1.120', 9001)
+            elif choice == '2':
+                try:
+                    self.exit_park_lights = ParkAccess("exit_queue_p")
+                    self.exit_plate_detection = PlateDetection("exit_queue", '192.168.1.120', 9001)
 
-                        thread3 = Thread(target=self.exit_plate_detection.read_stream)
-                        thread4 = Thread(target=self.exit_park_lights.loop)
+                    thread3 = Thread(target=self.exit_plate_detection.read_stream)
+                    thread4 = Thread(target=self.exit_park_lights.loop)
 
-                        thread3.start()
-                        thread4.start()
-
-                        self.thread_list.append(thread3)
-                        self.thread_list.append(thread4)
+                    thread3.start()
+                    thread4.start()
                         
-                    except AlprException:
-                        print("\nERRO: Verifique se o OpenALPR está corretamente instalado.\n")
+                except AlprException:
+                    print("\nERRO: Verifique se o OpenALPR está corretamente instalado.\n")
+                    print("Programa terminado.")
+                    os._exit(0)
 
-                    except ConnectionRefusedError:
-                        print("\nERRO: Verifique a ligação de rede com o computador responsável pela captura de imagens.\n")
+                except ConnectionRefusedError:
+                    print("\nERRO: Verifique a ligação de rede com o computador responsável pela captura de imagens.\n")
+                    print("Programa terminado.")
+                    os._exit(0)
 
-                elif choice == '0':
-                    if self.entry_plate_detection != None:
-                        self.entry_park_lights.running = False
-                        self.entry_plate_detection.close_program() 
-                        self.entry_park_lights.close_program()  
+                except IOError as e:
+                    if e.errno == 101:
+                        print("\nERRO: Verifique a sua conexão à rede.\n")
+                        print("Programa terminado.")
+                        os._exit(0)
 
-                        for t in self.thread_list:
-                            t.join()
+            elif choice == '0':                        
+                print("Programa terminado.")   
+                os._exit(0)
 
-                    elif self.exit_plate_detection != None:
-                        self.exit_park_lights.running = False
-                        self.exit_plate_detection.close_program()
-                        self.exit_park_lights.close_program()
-
-                        for t in self.thread_list:
-                            t.join()
-
-                    print("Programa terminado.")   
-                    
-                else:
-                    print("Escolha inválida, tente novamente.")
-
-            except KeyboardInterrupt:
+            else:
+                print("Escolha inválida.")
                 print("Programa terminado.")
-                sys.exit(0)
+                os._exit(0)
 
+        except KeyboardInterrupt:
+            print("Programa terminado.")
+            os._exit(0)
+
+        end_program = None
+        print("Para terminar o programa, introduza um input.\n")
+
+        try:
+            end_program = input()
+
+            if end_program != None:
+                print("Programa terminado.")
+                os._exit(0)
+        except KeyboardInterrupt:
+            print("Programa terminado.")
+            os._exit(0)
+        
 
 
 if __name__ == "__main__":
